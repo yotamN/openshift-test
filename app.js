a7f2f2b33ce9ae2a163522638dc05fbf8513bc33
Yotam Nachum <yotamnachum1@gmail.com>
2017-09-27 13:40:16 +0300

    Get the port from an env var

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 
+const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const IP = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8080, '0.0.0.0', function () {
+app.listen(PORT, IP, function () {
   console.log('Example app listening on port 3000!')
 })
