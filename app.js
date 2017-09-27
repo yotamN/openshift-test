c18db2483a762c93cb4dd00591e7459472ff21ff
Yotam Nachum <yotamnachum1@gmail.com>
2017-09-27 13:40:56 +0300

    Fixed loggin port

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get('/', function (req, res) {
 })
 
 app.listen(PORT, IP, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port ' + PORT + '}!')
 })
